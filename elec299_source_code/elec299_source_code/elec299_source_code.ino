b1e117199251720b39303da7088c441efdd5efa3
benjamintli <15btl1@queensu.ca>
2018-03-16 12:44:15 -0400

    initial commit with .ino file. added pin defines and setup commands

---
+++ elec299_source_code/elec299_source_code/elec299_source_code.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		elec299_source_code.ino
+ Created:	3/16/2018 12:36:35 PM
+ Author:	Benjamin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
